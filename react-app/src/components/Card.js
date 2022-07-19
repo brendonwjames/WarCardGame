a792082dac1f6f0053eca556f7bbf6b5796c7a70
Brendon <brendonwjames@gmail.com>
2022-07-19 12:44:47 -0700

    added default card back with conditional render

--- react-app/src/components/Card.js
+++ react-app/src/components/Card.js
@@ -1,16 +1,15 @@
 import React from "react";
-
-// import backCardImg from "../components/images/backCardImg.png";
 import heart from "../components/images/cards/Heart.jpg";
 import diamond from '../components/images/cards/Diamond.jpg';
 import club from '../components/images/cards/Club.jpg';
 import spade from '../components/images/cards/Spade.jpg';
+import background from '../components/images/cards/DefaultBackground.jpg';
 import './Card.css';
 
 const Card = ({ gameState }) => {
 
     const getCardSymbol = (suits) => {
-        let symbol;
+        let symbol = background;
         switch (suits) {
             case "Diamond":
                 return symbol = diamond;
@@ -25,13 +24,15 @@ const Card = ({ gameState }) => {
         };
     };
 
+    const imgState = !gameState.player1CardInfo.length ? 'card-back' : 'player-card-image'
+
     return (
         <div className='player-cards-container'>
             <div className='individual-card'>
-                {gameState.player1CardInfo && <img className='player-card-image' src={getCardSymbol(gameState.player1CardInfo[0])} alt='broken'></img>}
+                {<img className={imgState} src={getCardSymbol(gameState.player1CardInfo[0])} alt='broken'></img>}
             </div>
             <div className='individual-card'>
-                {gameState.player2CardInfo && <img className='player-card-image' src={getCardSymbol(gameState.player2CardInfo[0])} alt='broken'></img>}
+                {<img className={imgState} src={getCardSymbol(gameState.player2CardInfo[0])} alt='broken'></img>}
             </div>
         </div>
     );
